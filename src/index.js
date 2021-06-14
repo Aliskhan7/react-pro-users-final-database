ac1df640bfec276f797f00cd8521566da639f8d6
juleybib <aliskhangazamatov@gmail.com>
2021-06-14 18:51:37 +0300

    prettier

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-import store from "./redux";
-import App from "./components/App";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-      <Provider store={store}>
-          <BrowserRouter>
-              <App />
-          </BrowserRouter>
-      </Provider>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
 );

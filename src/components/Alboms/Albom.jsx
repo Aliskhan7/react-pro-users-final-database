c3e71a7c1add815d857928e919dbd766afbbb7eb
juleybib <aliskhangazamatov@gmail.com>
2021-06-15 15:41:39 +0300

    final proptypes

--- src/components/Alboms/Albom.jsx
+++ src/components/Alboms/Albom.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Albom(props) {
   return <li className="list-group-item">{props.albom.title}</li>;
 }
+Albom.propTypes = {
+  albom: PropTypes.object,
+};
 
 export default Albom;
--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import {NavLink, useParams} from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 function User(props) {
-    const id = parseInt(useParams().id)
+  const id = parseInt(useParams().id);
 
-    return (
-        <li className={`list-group-item ${id === props.user.id ? 'list-group-item-info' : ''}`}>
-            <NavLink className='text-decoration-none' to={`/${props.user.id}`}>
-                {props.user.name}
-            </NavLink>
-        </li>
-    );
+  return (
+    <li
+      className={`list-group-item ${
+        id === props.user.id ? 'list-group-item-info' : ''
+      }`}
+    >
+      <NavLink className="text-decoration-none" to={`/${props.user.id}`}>
+        {props.user.name}
+      </NavLink>
+    </li>
+  );
 }
-
+User.propTypes = {
+  user: PropTypes.object,
+};
 export default User;
